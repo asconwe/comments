7d3bb9f3e522cfa0a010477394744569be1e6535
August Conwell <august.conwell@gmail.com>
2017-06-15 17:29:46 -0400

    Added MONGODB_URI

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 // Database configuration
-mongoose.connect("mongodb://localhost/comments")
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI)
+} else {
+  mongoose.connect("mongodb://localhost/comments")
+}
 var db = mongoose.connection;
 
 // Show any mongoose errors
